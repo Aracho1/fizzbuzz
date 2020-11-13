cacf50c21be697773a5a421ef35509d81497d926
Aman Tank <AmanTank187@gmail.com>
2020-11-13 13:00:55 +0000

    Added more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,18 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when answer is 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizzbuzz" when answer is 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "1" when answer is 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+
 end
